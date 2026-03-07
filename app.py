806b840fb8dbb64c6c704502dc11e2e7be24e172
shrqbabu <techloot1100@gmail.com>
2026-03-07 23:34:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,78 @@
 from flask import Flask, render_template, request, redirect
-from db import mysql, init_db
+import pymysql
+import os
 
 app = Flask(__name__)
 
-init_db(app)
-
+# MySQL connection
+connection = pymysql.connect(
+    host="mysql.railway.internal",
+    user="root",
+    password="ccGrEaKeutTOFDmDGSstADzeuOVQffYt",
+    database="railway",
+    cursorclass=pymysql.cursors.DictCursor
+)
 
 # READ
 @app.route("/")
 def index():
-
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM test")
-
-    users = cur.fetchall()
-
-    cur.close()
-
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
     return render_template("index.html", users=users)
 
 
 # CREATE
 @app.route("/add", methods=["POST"])
 def add():
+    name = request.form["name"]
+    email = request.form["email"]
+
+    cursor = connection.cursor()
+    cursor.execute(
+        "INSERT INTO users(name,email) VALUES(%s,%s)",
+        (name,email)
+    )
+    connection.commit()
+
+    return redirect("/")
+
+
+# UPDATE
+@app.route("/update/<int:id>", methods=["POST"])
+def update(id):
 
     name = request.form["name"]
     email = request.form["email"]
 
-    cur = mysql.connection.cursor()
+    cursor = connection.cursor()
 
-    cur.execute("INSERT INTO test(name,email) VALUES(%s,%s)",(name,email))
+    cursor.execute(
+        "UPDATE users SET name=%s,email=%s WHERE id=%s",
+        (name,email,id)
+    )
 
-    mysql.connection.commit()
-    cur.close()
+    connection.commit()
 
     return redirect("/")
 
 
 # DELETE
-@app.route("/delete/<id>")
+@app.route("/delete/<int:id>")
 def delete(id):
 
-    cur = mysql.connection.cursor()
+    cursor = connection.cursor()
 
-    cur.execute("DELETE FROM test WHERE id=%s",(id,))
+    cursor.execute(
+        "DELETE FROM users WHERE id=%s",
+        (id,)
+    )
 
-    mysql.connection.commit()
-    cur.close()
+    connection.commit()
 
     return redirect("/")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
