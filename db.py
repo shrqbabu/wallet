7f05314aaa1d2e2b47fe401b64dc388409b33f0f
shrqbabu <techloot1100@gmail.com>
2026-03-07 22:44:13 +0530

    Update db.py

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ mysql = MySQL()
 
 def init_db(app):
 
-    app.config['MYSQL_HOST'] = 'switchback.proxy.rlwy.net'
+    app.config['MYSQL_HOST'] = 'mysql.railway.internal'
     app.config['MYSQL_USER'] = 'root'
     app.config['MYSQL_PASSWORD'] = 'ueNfAawdvFLbtEFZvhrMbzfnIKoUsRlS'
     app.config['MYSQL_DB'] = 'railway'
